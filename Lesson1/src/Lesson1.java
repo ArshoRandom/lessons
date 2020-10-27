4a8218323db660e841587aa2f0136aba90cad609
ArshoRandom <papoyanarshak@gmail.com>
2020-10-27 07:44:02 +0400

    lesson 1

---
+++ Lesson1/src/Lesson1.java
@@ -0,0 +1,34 @@
+public class Lesson1 {
+
+    public static void main(String[] args) {
+
+        byte b = 127;
+        System.out.println("Byte - " + b);
+        int i = 31254;
+        System.out.println("Integer - " + i);
+        long l = 15899747455L;
+        System.out.println("Long - " + l);
+
+
+
+        double d = 15.25;
+        System.out.println("Double - " + d);
+        float f = 12.8888F;
+        System.out.println("Float - " + f);
+
+
+        char c = 65;
+        System.out.println("Character from integer - " + c);
+
+        char c2 = '\u0077';
+        System.out.println("Character from unicode - " + c2);
+
+        char c3 = 'y';
+        System.out.println("Character from symbol - " + c3);
+
+
+        String str = "This is string";
+        System.out.println(str);
+
+    }
+}
